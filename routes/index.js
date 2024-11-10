14f9610282c1190375cd537d6a6081a082f99c2c
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-11-10 18:53:21 +0100

    fix env

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const api = `/${apiURL}`;
 
 // Welcome route
 router.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the API (api.lebondeveloppeur.net) !' });
+    res.json({ message: 'Welcome to the API !' });
 });
 
   
